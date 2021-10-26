06b58148944d752fd168347a32030e77e23296da
Mohammed <mohammedmaheswer12@gmail.com>
2021-10-26 13:51:19 +0530

    two file commited

---
+++ exceldwld.php
@@ -0,0 +1,24 @@
+<?php
+ header('Content-type: application/xml');
+ header('content-disposition:attachment;filename= file.xls');
+$conn = mysqli_connect('localhost',"root","","ajax_tutorial") or die('failed');
+$query = "select * from ajax";
+$exec = mysqli_query($conn,$query);
+
+
+$response = "<table style=''><tr> <th style='width:10%'>dname</th><th>email</th><th>password</th></tr>";
+while($fetch = mysqli_fetch_assoc($exec)){
+    $response .= "<tr><td>$fetch[name]</td><td>$fetch[email]</td><td>$fetch[password]</td><td><button  class='btn-sm btn-primary edit'  data-id='$fetch[id]'  data-toggle='modal' data-target='#exampleModal1'>Edit</button> <button  data-id='$fetch[id]'  class='btn-sm btn-danger delete'>Delete</button></td></tr>";
+}
+ echo $response  .= "</table>";
+$arr = [
+    'one' => ["moon" => 'oneday',"loon" => 'oneday'],
+    'two' => ["moon" => 'oneday',"loon" => 'oneday'],
+    'three' => ["moon" => 'oneday',"loon" => 'oneday'],
+    ];
+// echo  json_encode($response);
+
+
+
+
+?>
